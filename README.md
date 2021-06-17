f609bbe85035104c263ac08616bd41a12bbb4c4e
miltonospina <miltonospina@gmail.com>
2021-06-17 15:24:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-ogvgw7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ogvgw7)
---
+++ index.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+
+const deco = (): ClassDecorator => {
+  return target => {
+    console.log(Reflect.getMetadata('design:paramtypes', target));
+  };
+};
+
+class Primi {
+  constructor(private nombre) {}
+}
+
+@deco()
+class Decorada {
+  constructor(private param: number, primi: Primi) {}
+}
+
+new Decorada(1, new Primi(1));
